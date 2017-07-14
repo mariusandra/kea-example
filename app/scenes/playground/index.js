647bbf7aaac17383440273b376b581c99160fd88
Marius Andra <marius.andra@gmail.com>
2017-07-14 11:46:41 +0200

    playground test sagas

--- app/scenes/playground/index.js
+++ app/scenes/playground/index.js
@@ -3,6 +3,17 @@ import './styles.scss'
 import React, { Component } from 'react'
 import { kea } from 'kea'
 
+const sagaLogic = kea({
+  start: function * () {
+    console.log('started scene, mounted component')
+  }
+})
+
+@kea({
+  sagas: [
+    sagaLogic.saga
+  ]
+})
 export default class Playground extends Component {
   render () {
     return (
