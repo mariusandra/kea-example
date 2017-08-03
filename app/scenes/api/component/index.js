c28aeb4ee21ec5b5afdc16b5b4e70c0086f23c84
Marius Andra <marius.andra@gmail.com>
2017-08-03 23:16:33 +0200

    props in wrapped selectors

--- app/scenes/api/component/index.js
+++ app/scenes/api/component/index.js
@@ -8,7 +8,8 @@ const code = {
   usage: require('raw-loader!./code/usage.txt'),
   decorators: require('raw-loader!./code/decorators.txt'),
   path: require('raw-loader!./code/path.txt'),
-  key: require('raw-loader!./code/key.txt')
+  key: require('raw-loader!./code/key.txt'),
+  selectors: require('raw-loader!./code/selectors.txt')
 }
 
 @kea({})
@@ -30,7 +31,7 @@ export default class API extends Component {
           Wrap a kea logic store around a React component.
         </p>
         <p>
-          The component will receive all selectors in <code>this.props</code> and all actions under <code>this.actions</code>.
+          The React component will receive all of the logic store's selectors in <code>this.props</code> and all actions under <code>this.actions</code>.
         </p>
         <Highlight className='javascript'>{code.usage}</Highlight>
         <p>
@@ -48,16 +49,25 @@ export default class API extends Component {
         <p>
           These options are different:
         </p>
+
         <h4>key: <code>(props) => 'key'</code></h4>
         <p>
           If you wish, you may define a <code>key</code> that distinguishes instances of the component
         </p>
         <Highlight className='javascript'>{code.key}</Highlight>
+
         <h4>path: <code>(key) => []</code></h4>
         <p>
           The path takes the key as an argument if you wish to define the location in redux for the component instance
         </p>
         <Highlight className='javascript'>{code.path}</Highlight>
+
+        <h4>selectors: <code>{'({ path, constants, actions, selectors }) => ({})'}</code></h4>
+        <p>
+          The <code>selectors</code> have an additional feature with wrapped logic stores. You can access the <code>props</code> passed to
+          your component like so:
+        </p>
+        <Highlight className='javascript'>{code.selectors}</Highlight>
       </div>
     )
   }
