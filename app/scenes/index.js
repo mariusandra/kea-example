14ccdb305b91d73430a8255b5b8f1b9140635228
Marius Andra <marius.andra@gmail.com>
2017-08-14 10:54:47 +0200

    redirects

--- app/scenes/index.js
+++ app/scenes/index.js
@@ -9,35 +9,38 @@ import asyncComponent from '~/utils/async-component'
 import WithHeader from '~/components/header'
 
 // all the root components in this app
-export const Homepage = asyncComponent('Homepage', () => import(/* webpackChunkName: "homepage" */'./homepage'))
+const Homepage = asyncComponent('Homepage', () => import(/* webpackChunkName: "homepage" */'./homepage'))
 
-export const GuideInstallation = asyncComponent('GuideInstallation', () => import(/* webpackChunkName: "guideInstallation" */'./guide/installation'))
-export const GuideCounter = asyncComponent('GuideCounter', () => import(/* webpackChunkName: "guideCounter" */'./guide/counter'))
-export const GuideCounterDynamic = asyncComponent('GuideCounterDynamic', () => import(/* webpackChunkName: "guideCounterDynamic" */'./guide/counter-dynamic'))
-export const GuideSliders = asyncComponent('GuideSliders', () => import(/* webpackChunkName: "guideSliders" */'./guide/sliders'))
-export const GuideGithub = asyncComponent('GuideGithub', () => import(/* webpackChunkName: "guideGithub" */'./guide/github'))
-export const GuideConnected = asyncComponent('GuideConnected', () => import(/* webpackChunkName: "guideConnected" */'./guide/connected'))
-export const GuideConnectedServices = asyncComponent('GuideConnectedServices', () => import(/* webpackChunkName: "guideConnectedServices" */'./guide/connected-services'))
-export const GuideMigration = asyncComponent('GuideMigration', () => import(/* webpackChunkName: "guideMigration" */'./guide/migration'))
+const guideRedirect = () => (<Redirect to='/guide/installation' />)
+const GuideInstallation = asyncComponent('GuideInstallation', () => import(/* webpackChunkName: "guideInstallation" */'./guide/installation'))
+const GuideCounter = asyncComponent('GuideCounter', () => import(/* webpackChunkName: "guideCounter" */'./guide/counter'))
+const GuideCounterDynamic = asyncComponent('GuideCounterDynamic', () => import(/* webpackChunkName: "guideCounterDynamic" */'./guide/counter-dynamic'))
+const GuideSliders = asyncComponent('GuideSliders', () => import(/* webpackChunkName: "guideSliders" */'./guide/sliders'))
+const GuideGithub = asyncComponent('GuideGithub', () => import(/* webpackChunkName: "guideGithub" */'./guide/github'))
+const GuideConnected = asyncComponent('GuideConnected', () => import(/* webpackChunkName: "guideConnected" */'./guide/connected'))
+const GuideConnectedServices = asyncComponent('GuideConnectedServices', () => import(/* webpackChunkName: "guideConnectedServices" */'./guide/connected-services'))
+const GuideMigration = asyncComponent('GuideMigration', () => import(/* webpackChunkName: "guideMigration" */'./guide/migration'))
 
-export const ApiLogic = asyncComponent('ApiLogic', () => import(/* webpackChunkName: "apiLogic" */'./api/logic'))
-export const ApiComponent = asyncComponent('ApiComponent', () => import(/* webpackChunkName: "apiComponent" */'./api/component'))
-export const ApiConnect = asyncComponent('ApiConnect', () => import(/* webpackChunkName: "apiConnect" */'./api/connect'))
-export const ApiReducer = asyncComponent('ApiReducer', () => import(/* webpackChunkName: "apiReducer" */'./api/reducer'))
-export const ApiSaga = asyncComponent('ApiSaga', () => import(/* webpackChunkName: "apiSaga" */'./api/saga'))
-export const ApiAction = asyncComponent('ApiAction', () => import(/* webpackChunkName: "apiAction" */'./api/action'))
+const apiRedirect = () => (<Redirect to='/api/logic' />)
+const ApiLogic = asyncComponent('ApiLogic', () => import(/* webpackChunkName: "apiLogic" */'./api/logic'))
+const ApiComponent = asyncComponent('ApiComponent', () => import(/* webpackChunkName: "apiComponent" */'./api/component'))
+const ApiConnect = asyncComponent('ApiConnect', () => import(/* webpackChunkName: "apiConnect" */'./api/connect'))
+const ApiReducer = asyncComponent('ApiReducer', () => import(/* webpackChunkName: "apiReducer" */'./api/reducer'))
+const ApiSaga = asyncComponent('ApiSaga', () => import(/* webpackChunkName: "apiSaga" */'./api/saga'))
+const ApiAction = asyncComponent('ApiAction', () => import(/* webpackChunkName: "apiAction" */'./api/action'))
 
-export const ExampleTodos = asyncComponent('ExampleTodos', () => import(/* webpackChunkName: "exampleTodos" */'./examples/todos'))
-export const ExampleGithub = asyncComponent('ExampleGithub', () => import(/* webpackChunkName: "exampleGithub" */'./examples/github'))
+const examplesRedirect = () => (<Redirect to='/examples/todos' />)
+const ExamplesTodos = asyncComponent('ExamplesTodos', () => import(/* webpackChunkName: "examplesTodos" */'./examples/todos'))
+const ExamplesGithub = asyncComponent('ExamplesGithub', () => import(/* webpackChunkName: "examplesGithub" */'./examples/github'))
 
-export const Playground = asyncComponent('Playground', () => import(/* webpackChunkName: "playground" */'./playground'))
+const Playground = asyncComponent('Playground', () => import(/* webpackChunkName: "playground" */'./playground'))
 
 export default () => (
   <WithHeader>
     <div>
       <Route exact path='/' component={Homepage} />
 
-      <Route path='/guide'><Redirect to='/guide/installation' /></Route>
+      <Route path='/guide' exact render={guideRedirect} />
       <Route path='/guide/installation' component={GuideInstallation} />
       <Route path='/guide/counter' component={GuideCounter} />
       <Route path='/guide/counter-dynamic' component={GuideCounterDynamic} />
@@ -47,7 +50,7 @@ export default () => (
       <Route path='/guide/connected-services' component={GuideConnectedServices} />
       <Route path='/guide/migration' component={GuideMigration} />
 
-      <Route path='/api'><Redirect to='/api/logic' /></Route>
+      <Route path='/api' exact render={apiRedirect} />
       <Route path='/api/logic' component={ApiLogic} />
       <Route path='/api/component' component={ApiComponent} />
       <Route path='/api/connect' component={ApiConnect} />
@@ -55,9 +58,9 @@ export default () => (
       <Route path='/api/saga' component={ApiSaga} />
       <Route path='/api/action' component={ApiAction} />
 
-      <Route path='/examples'><Redirect to='/examples/todos' /></Route>
-      <Route path='/examples/todos' component={ExampleTodos} />
-      <Route path='/examples/github' component={ExampleGithub} />
+      <Route path='/examples' exact render={examplesRedirect} />
+      <Route path='/examples/todos' component={ExamplesTodos} />
+      <Route path='/examples/github' component={ExamplesGithub} />
 
       <Route path='/playground' component={Playground} />
     </div>
