63fbac31b7d79c648cb321ffdf5676e9c06d2b7a
Marius Andra <marius.andra@gmail.com>
2020-05-12 14:00:18 +0200

    add banner that these are old docs

--- app/scenes/homepage/index.js
+++ app/scenes/homepage/index.js
@@ -108,7 +108,8 @@ export default class HomepageScene extends Component {
           </div>
         </div>
         <div className='description'>
-          <p><strong>NB!</strong> This documentation is for the 1.0 release. To see docs for 0.28, <a href='https://5d7a15398fdb1e00073ed27f--kea.netlify.com/'>click here</a>.</p>
+          <p><strong>NB!</strong> This documentation is for the 1.0 release. To see docs for 0.28, <a href='https://5d7a15398fdb1e00073ed27f--kea-v1.netlify.com/'>click here</a>.</p>
+          <p>To see the docs for 2.0, <a href='https://kea.js.org'>click here</a>.</p>
         </div>
         <h2>Why Kea?</h2>
         <div className='description'>
